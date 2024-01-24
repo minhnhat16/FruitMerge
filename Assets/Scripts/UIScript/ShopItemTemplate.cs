ac9287a6bb441bd29d18406db793eeeb701722a3
cyo2 <108583303+cyo2@users.noreply.github.com>
2024-01-25 00:56:24 +0700

    change 24.1

---
+++ Assets/Scripts/UIScript/ShopItemTemplate.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShopItemTemplate : MonoBehaviour
+{
+    [SerializeField] private Image backGround;
+    [SerializeField] private Image itemImg;
+    [SerializeField] private Text name_lb;
+    [SerializeField] private Image ContainBox;
+    [SerializeField] private Text total_lb;
+    [SerializeField] private Text cost_lb;
+
+
+
+}
