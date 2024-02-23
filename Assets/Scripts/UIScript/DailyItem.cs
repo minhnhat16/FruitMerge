a239005061b23c4d522d7f3207aa6cac264e2bb0
cyo2 <108583303+cyo2@users.noreply.github.com>
2024-02-23 14:53:01 +0700

    change 23.02

---
+++ Assets/Scripts/UIScript/DailyItem.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DailyItem : MonoBehaviour
+{
+    public List<Image> backgrounds = new List<Image>();
+    public Image itemImg;
+    public TextMeshProUGUI day_lb;
+    public TextMeshProUGUI Amount_lb;
+    public 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
